25e22e4855c03909185a0a3e22105f821986b392
Ferdian Ahmad R <ferdianahmadrozikin10@gmail.com>
2022-03-10 13:10:34 +0700

    add queryclient and hydrate state

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,22 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { ChakraProvider, theme } from '@chakra-ui/react'
+import { QueryClient, QueryClientProvider } from "react-query"
+import { ReactQueryDevtools } from "react-query/devtools"
+import { Hydrate } from 'react-query/hydration'
+
+const queryClients = new QueryClient()
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider resetCSS={true} theme={theme}>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <QueryClientProvider client={queryClients}>
+      <Hydrate state={pageProps.dehydratedState}>
+        <ChakraProvider resetCSS={true} theme={theme}>
+          <Component {...pageProps} />
+        </ChakraProvider>
+      </Hydrate>
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   )
 }
 
